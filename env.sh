d929d25961de0b81e406361e4c805d5a7771e8e4
Jean Benitez <jkbdiaz@gmail.com>
2019-05-18 01:03:00 -0500

    :up: Abort installation and start if isn't installed required dependencies on machine

--- env.sh
+++ env.sh
@@ -4,3 +4,17 @@ export DB_NAME="servercheck"
 export DB_USER="root"
 export DB_PASS=""
 export GOPORT=8005
+
+# checking requered dependencies
+if ! [ -x "$(command -v go)" ]; then
+  echo 'Error: go is not installed.' >&2
+  exit 1
+fi
+if ! [ -x "$(command -v dep)" ]; then
+  echo 'Error: dep is not installed.' >&2
+  exit 1
+fi
+if ! [ -x "$(command -v cockroach)" ]; then
+  echo 'Error: cockroach is not installed.' >&2
+  exit 1
+fi
